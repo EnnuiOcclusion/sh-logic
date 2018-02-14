879e8eaa9a42d1ee7a81c4a9359feaddb8b84d43
Peter Morris <morris.com@gmail.com>
2018-02-13 19:45:31 -0800

    Made a general sorting function and switched to using it.

--- src/sh_logic/core.clj
+++ src/sh_logic/core.clj
@@ -16,28 +16,46 @@
   Two are not in the right row
   One of the numbers is the final key")
 
+(defn tester []
+  (run* [q]
+        (fresh [a]
+               (== a 1)
+               (conda
+                ((== a 1) (== q 2))
+                (s# (== q 3))))))
+(defn sorto [l]
+  (conda
+   [(nilo l)]
+   [(emptyo (rest l))]
+   [(fd/< (first l) (first (rest l))) (sorto (rest l))]))
+
 (defn solve-puzzle []
   (run* [q]
-        (fresh [a b c d]
-               (== [a b c d] q)
-               (everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) [a b c d])
-               (fd/> a b)
-               (fd/* c 2 b)
-               (fd/< c d)
-               (fd/* d 2 a)
+    (fresh [a b c d]
+      (== [a b c d] q)
+      (everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) [a b c d])
+      (fd/> a b)
+      (fd/* c 2 b)
+      (fd/< c d)
+      (fd/* d 2 a)
 
-               (fd/distinct [a b c d])
+      (fd/distinct [a b c d])
 
-               (fresh [l1 l2 l3 l4]
-                      (everyg #(membero % q) [l1 l2 l3 l4])
-                      (fd/distinct [l1 l2 l3 l4])
-                      (fd/< l1 l2)
-                      (fd/< l1 l3)
-                      (fd/< l1 l4)
-                      (fd/< l2 l3)
-                      (fd/< l2 l4)
-                      (fd/< l3 l4)
-                      (fd/> l2 3)))))
+      ;; Old and busted
+      ;; ----------
+      ;; (fresh [l1 l2 l3 l4]
+      ;; (everyg #(membero % q) [l1 l2 l3])
+      ;; (fd/distinct [l1 l2 l3])
+      ;; (everyg #(fd/in % (fd/domain 4 5 6 7 8 9)) [l1 l2 l3]))
+
+      ;; The New Hotness
+      ;; ----------
+      (fresh [l1 l2 l3 l4]
+        (everyg #(membero % q) [l1 l2 l3 l4])
+        (fd/distinct [l1 l2 l3 l4])
+        (sorto [l1 l2 l3 l4])
+        (fd/> l2 3)))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
